e7244c8e094f74790774b51e391bdc2f961223db
Ian Clarkson <clarksonian@gmail.com>
2019-05-06 23:22:52 -0700

    Properly register plugins directory and we generate a (weird) png

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
-#include <mapnik/map.hpp>
-#include <mapnik/load_map.hpp>
 #include <mapnik/agg_renderer.hpp>
+#include <mapnik/datasource_cache.hpp>
 #include <mapnik/image.hpp>
 #include <mapnik/image_util.hpp>
+#include <mapnik/load_map.hpp>
+#include <mapnik/map.hpp>
 
 int main()
 {
-    mapnik::Map m(256,256);
-    mapnik::load_map(m, "example.xml");
-    m.zoom_all();
-    mapnik::image_rgba8 im(256,256);
-    mapnik::agg_renderer<mapnik::image_rgba8> ren(m, im);
-    ren.apply();
-    mapnik::save_to_file(im, "the_image.png");
+  mapnik::datasource_cache::instance().register_datasources("/usr/local/lib/mapnik/input");
+
+  mapnik::Map m(1024,1024);
+  mapnik::load_map(m, "example.xml");
+  m.zoom_all();
+  mapnik::image_rgba8 im(1024,1024);
+  mapnik::agg_renderer<mapnik::image_rgba8> ren(m, im);
+  ren.apply();
+  mapnik::save_to_file(im, "the_image.png");
 }
